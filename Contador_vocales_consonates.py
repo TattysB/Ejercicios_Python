5b105b209171ed411952bc835da75bf334c53b83
Tatiana Bayona <tatianabayona2017@gmail.com>
2025-08-01 13:32:28 -0500

    Ejercicio de contador de vocales y consonantes

--- Contador_vocales_consonates.py
+++ Contador_vocales_consonates.py
@@ -3,7 +3,36 @@
 #cuántas consonantes contiene la frase (sin contar espacios ni
 #símbolos) y mostrar los resultados.
 
-frase=input("Ingresa una frase: ")
+
+frase=str.lower(input("Ingresa una frase: "))
+frase=frase.replace(" ","")
+vocal="aeiou"
+contador_vocales=0
+contador_consonantes=0
+
+def contador (frase):
+    """
+    Esta función nos sirve para contar cuantas vocales hay en una frase
+    y cuantas consonantes hay,esto ignora los espacios y caracteres especiales
+
+    Args:
+        frase (str): Texto de la frase
+        contador_vocales (int): contador de vocales
+        contador_consonantes (int): contador de consonantes
+    Returns:
+        La cantidad de vocales (int)
+        La cantidad de consonantes (int)
+
+    """
 
 
-for
+for i  in frase:
+    if i in vocal:
+        contador_vocales+=1
+        if i in vocal:
+           contador_consonantes+=1
+
+print(f"Vocales: {contador_vocales} consonantes: {contador_consonantes}")
+
+
+
