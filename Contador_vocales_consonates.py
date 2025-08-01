fd35870e6b61d230661e78a2802fd9728e76d53d
Tatiana Bayona <tatianabayona2017@gmail.com>
2025-08-01 12:20:24 -0500

    Se resuelve los ejercicos de IMC,verificador edad,tabla multiplicacion

---
+++ Contador_vocales_consonates.py
@@ -0,0 +1,9 @@
+#Desarrolla un programa que pida al usuario una frase.
+#El programa debe contar cuántas vocales y
+#cuántas consonantes contiene la frase (sin contar espacios ni
+#símbolos) y mostrar los resultados.
+
+frase=input("Ingresa una frase: ")
+
+
+for
